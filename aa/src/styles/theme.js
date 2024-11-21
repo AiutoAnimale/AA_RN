af3095f06de88db5aa7f0fc5906f91b49457becc
downpour07 <choiheeyoung10@gmail.com>
2024-11-21 23:27:02 +0900

    ⚙️ :: color system

---
+++ aa/src/styles/theme.js
@@ -0,0 +1,6 @@
+export const color = {
+    White: "#FFFFFF",
+    Black: "#000000",
+    Gray: ["#F3F3F3", "#DFDFDF", "#D6D6D6", "#CACACA", "#B5B5B5", "#B0B0B0", "#989898", "#898989", "#5A5A5A", "#353535"],
+    Orange: ["#FFF0E7", "#FFD4BD", "#FFB993", "#FF9E69", "#FF823F", "#D64D02", "#AD3D00"],
+};
