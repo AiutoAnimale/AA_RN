c84d9d608c30c9378dd2a957b37fe0d2193f29b0
downpour07 <choiheeyoung10@gmail.com>
2024-11-22 17:13:01 +0900

    📑 :: Splash

---
+++ aa/src/screens/Loading/index.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { View, StyleSheet } from "react-native";
+import { color } from "../../styles/theme";
+import constants from "../../styles/constants";
+
+import Logo from "../../assets/icon/Logo";
+
+const Loading = ({navigation}) => {
+
+    return(
+        <View style={Styles.container}>
+            <Logo></Logo>
+        </View>
+    );
+}
+
+const Styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: constants.width,
+        height: constants.height,
+        backgroundColor: color.White,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
+
+export default Loading;
