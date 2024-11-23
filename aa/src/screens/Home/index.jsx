c1c66c20bda2bc655a17f4a3d64c97ec0605d540
downpour07 <choiheeyoung10@gmail.com>
2024-11-23 22:01:20 +0900

    📝 :: modal 추가

--- aa/src/screens/Home/index.jsx
+++ aa/src/screens/Home/index.jsx
@@ -1,15 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import { View, Image, Keyboard, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useState } from 'react';
+import { View, Image, Modal, StyleSheet, TouchableOpacity } from "react-native";
 import { color } from "../../styles/theme";
 import constants from "../../styles/constants";
 import CustomText from "../../styles/customText";
 
 import Header from "../../components/Header";
 import Q from "../../assets/icon/Q";
+import NO from "../../assets/icon/NO";
+import OK from "../../assets/icon/OK";
 
-const HomePage = ({navigation}) => {
+const HomePage = ({ navigation }) => {
+    const [isModalVisible, setModalVisible] = useState(false);
+    const [modalState, setModalState] = useState(false);
 
-    return(
+    const handleAnswer = (isCorrect) => {
+        setModalState(isCorrect);
+        setModalVisible(true);
+    };
+
+    return (
         <View style={Styles.container}>
             <Header />
             <Image style={Styles.img} source={require('../../assets/image/weather.png')} />
@@ -26,23 +35,56 @@ const HomePage = ({navigation}) => {
                 <CustomText style={Styles.quizLight}>문제를 풀고 레벨을 올려보세요!</CustomText>
                 <CustomText style={Styles.q}>다음 중 한국에서 반려동물 유기 문제와 관련하여 가장 정확한 설명은?</CustomText>
                 <View style={Styles.btnContainer}>
-                    <TouchableOpacity style={Styles.btn}>
+                    <TouchableOpacity
+                        style={Styles.btn}
+                        onPress={() => handleAnswer(false)}
+                    >
                         <CustomText style={Styles.btnText}>2019년 기준 약 1만 5천마리이다.</CustomText>
                     </TouchableOpacity>
-                    <TouchableOpacity style={Styles.btn}>
-                        <CustomText style={Styles.btnText}>2019년 기준 약 1만 5천마리이다.</CustomText>
+                    <TouchableOpacity
+                        style={Styles.btn}
+                        onPress={() => handleAnswer(true)}
+                    >
+                        <CustomText style={Styles.btnText}>2019년 기준 약 13만 마리이다.</CustomText>
                     </TouchableOpacity>
-                    <TouchableOpacity style={Styles.btn}>
-                        <CustomText style={Styles.btnText}>2019년 기준 약 1만 5천마리이다.</CustomText>
+                    <TouchableOpacity
+                        style={Styles.btn}
+                        onPress={() => handleAnswer(false)}
+                    >
+                        <CustomText style={Styles.btnText}>유기동물 대부분은 즉시 새로운 주인에게 입양된다.</CustomText>
                     </TouchableOpacity>
-                    <TouchableOpacity style={Styles.btn}>
-                        <CustomText style={Styles.btnText}>2019년 기준 약 1만 5천마리이다.</CustomText>
+                    <TouchableOpacity
+                        style={Styles.btn}
+                        onPress={() => handleAnswer(false)}
+                    >
+                        <CustomText style={Styles.btnText}>유기동물 보호 시설 환경이 매우 우수하다.</CustomText>
                     </TouchableOpacity>
                 </View>
             </View>
+            <Modal
+                transparent={true}
+                visible={isModalVisible}
+                animationType="fade"
+                onRequestClose={() => setModalVisible(false)}
+            >
+                <View style={Styles.modalOverlay}>
+                    <View style={Styles.modalContent}>
+                        {modalState ? <OK /> : <NO />}
+                        <CustomText style={Styles.modalText}>
+                            {modalState ? "정답이에요! 날카로운 통찰력에 감탄했어요!" : "정답은 ~이예요!"}
+                        </CustomText>
+                        <TouchableOpacity
+                            style={Styles.modalButton}
+                            onPress={() => setModalVisible(false)}
+                        >
+                            <CustomText style={Styles.modalButtonText}>확인</CustomText>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
         </View>
     );
-}
+};
 
 const Styles = StyleSheet.create({
     container: {
@@ -54,54 +96,54 @@ const Styles = StyleSheet.create({
     },
     img: {
         width: constants.width,
-        height: constants.height/4.5
+        height: constants.height / 4.5
     },
     Top: {
         position: 'absolute',
-        top: constants.height/8,
-        left: constants.width/15,
+        top: constants.height / 8,
+        left: constants.width / 15,
         fontSize: 20,
         fontWeight: '700',
         color: color.White
     },
     Bottom: {
         position: 'absolute',
-        top: constants.height/6.5,
-        left: constants.width/15,
+        top: constants.height / 6.5,
+        left: constants.width / 15,
         fontSize: 20,
         fontWeight: '700',
         color: color.White
     },
     tag: {
         position: 'absolute',
-        top: constants.height/12,
-        left: constants.width/15,
+        top: constants.height / 12,
+        left: constants.width / 15,
         fontSize: 12,
         color: color.Gray[8],
         backgroundColor: color.White,
         borderRadius: 6,
-        width: constants.width/3.5,
-        height: constants.height/32,
+        width: constants.width / 3.5,
+        height: constants.height / 32,
         textAlign: 'center',
         textAlignVertical: 'center',
     },
     temp: {
         position: 'absolute',
-        top: constants.height/8,
-        left: constants.width/1.5,
+        top: constants.height / 8,
+        left: constants.width / 1.5,
         fontSize: 50,
         color: color.White
     },
     data: {
         position: 'absolute',
-        top: constants.height/5,
-        left: constants.width/15,
+        top: constants.height / 5,
+        left: constants.width / 15,
         fontSize: 14,
         color: color.White
     },
     dataContainer: {
-        width: constants.width/1.1,
-        height: constants.height/2,
+        width: constants.width / 1.1,
+        height: constants.height / 2,
         padding: 10,
         paddingTop: 30,
         rowGap: 10,
@@ -113,21 +155,22 @@ const Styles = StyleSheet.create({
         gap: 10
     },
     quiz: {
-        fontSize: 18,
+        fontSize: 20,
         fontWeight: '700',
     },
     quizLight: {
-        fontSize: 13,
+        fontSize: 14,
         color: color.Gray[8],
     },
     q: {
-        fontSize: 16,
-        fontWeight: '600',
-        color: color.Black
+        fontSize: 17,
+        fontWeight: '700',
+        color: color.Black,
+        marginTop: 20
     },
     btn: {
-        width: constants.width/1.2,
-        height: constants.height/17,
+        width: constants.width / 1.2,
+        height: constants.height / 17,
         borderWidth: 1,
         borderColor: color.Gray[2],
         borderRadius: 10,
@@ -144,7 +187,41 @@ const Styles = StyleSheet.create({
     btnContainer: {
         marginTop: 30,
         rowGap: 15
-    }
-})
+    },
+    modalOverlay: {
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modalContent: {
+        width: constants.width / 1.3,
+        height: constants.height / 3.5,
+        backgroundColor: color.White,
+        borderRadius: 10,
+        padding: 20,
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
+    modalText: {
+        fontSize: 16,
+        color: color.Gray[8],
+        textAlign: 'center',
+    },
+    modalButton: {
+        backgroundColor: color.Orange[4],
+        width: constants.width / 2,
+        height: constants.height / 18,
+        borderRadius: 10,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    modalButtonText: {
+        color: color.White,
+        fontWeight: '700',
+        fontSize: 17,
+    },
+});
 
 export default HomePage;
