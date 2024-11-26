2d7ad695964e882f62fc8e6a874d7ba86a9e618b
downpour07 <choiheeyoung10@gmail.com>
2024-11-26 19:06:25 +0900

    📑 :: weather

--- aa/src/apis/weather.jsx
+++ aa/src/apis/weather.jsx
@@ -3,12 +3,17 @@ import { OPEN_API_KEY, OPEN_SERVICE_KEY } from "@env";
 
 const onWeather = async ( ) => {
 
-    console.log(OPEN_API_KEY)
-    console.log(OPEN_SERVICE_KEY)
+    // console.log(OPEN_API_KEY)
+    // console.log(OPEN_SERVICE_KEY)
+
+    let now = new Date();
+    let year = now.getFullYear();
+    let todayMonth = now.getMonth()+1;
+    let todayDate = now.getDate();
+    let hours = now.getHours()+9;
 
     try {
-        const response = await axios.get(`${OPEN_API_KEY}?serviceKey=${OPEN_SERVICE_KEY}&numOfRows=1&pageNo=1&dataType=json&base_date=20241125&base_time=2000&nx=56&ny=71`);
-
+        const response = await axios.get(`${OPEN_API_KEY}?serviceKey=${OPEN_SERVICE_KEY}&numOfRows=1&pageNo=1&dataType=json&base_date=${year}${todayMonth}${todayDate}&base_time=0800&nx=56&ny=71`);
         console.log(response.data.response.body.items.item[0].fcstValue);
 
         return response.data.response.body.items.item[0].fcstValue;
